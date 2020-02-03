905a68d5b20a676955fb3db0b1401badb03fe0b0
Francisco Ramos <jscriptcoder@gmail.com>
2020-02-03 12:42:54 +0100

    WIP: still trying to solve LunarLander

--- test_env.py
+++ test_env.py
@@ -2,8 +2,9 @@ import gym
 import gym.spaces
 import rocket_lander_gym
 
-env = gym.make("RocketLander-v0")
+env = gym.make("LunarLander-v2") # RocketLander-v0 | LunarLander-v2 | MountainCar-v0 | CartPole-v0
 env.reset()
+step = 0
 
 PRINT_DEBUG_MSG = True
 
@@ -12,8 +13,10 @@ while True:
     env.render()
     action = env.action_space.sample()
     observation, reward, done, info = env.step(action)
+    step += 1
 
     if PRINT_DEBUG_MSG:
+        print("Step          ", step)
         print("Action Taken  ", action)
         print("Observation   ", observation)
         print("Reward Gained ", reward)
